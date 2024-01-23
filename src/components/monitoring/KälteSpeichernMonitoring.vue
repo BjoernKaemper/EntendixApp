2ca4af97c98eb3baa8d036d4816f92ba34478bf2
Maximilian Both <maximilian_alexander.both@th-koeln.de>
2024-01-23 12:54:26 +0100

    add kälte to monitoring

--- src/components/monitoring/KälteSpeichernMonitoring.vue
+++ src/components/monitoring/KälteSpeichernMonitoring.vue
@@ -11,7 +11,7 @@
           <KpisMonitoringAnlage grundfunktion="Kälte"/>
         </v-container>
         <v-container>
-          <LineChartAll :allElements="this.allSes" zweiteFunktion="KälteVerteilen"/> 
+          <LineChartAll :allElements="this.allSes" zweiteFunktion="KälteSpeichern"/> 
         </v-container>
         <v-container>
           <v-row>
@@ -21,23 +21,43 @@
               variant="outlined" class="pa-4 anlagen-card">
               <v-card-text class="center-content">
                   <v-container class="mx-0 mx-lg-10 mx-xl-16 px-lg-10 px-xl-16">
-                    <svg id="Ebene_1" data-name="Ebene 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 89.97 94.32">
-                      <g id="Ebene_1-2" data-name="Ebene_1">
-                        <line class="cls-2" y1="85.74" x2="89.97" y2="85.74"/>
-                        <line class="cls-4" y1="92.82" x2="89.91" y2="92.82"/>
-                        <polyline class="cls-3" points="65.13 92.82 65.13 10.99 59.75 10.99"/>
-                        <polyline class="cls-1" points="65.13 92.82 65.13 10.99 59.75 10.99"/>
-                        <circle class="cls-6" cx="46.52" cy="11.84" r="7.8"/>
-                        <circle class="cls-6" cx="46.52" cy="11.84" r="11.34"/>
-                        <circle class="cls-6" cx="28.66" cy="39.84" r="6.38"/>
-                        <polyline class="cls-3" points="35.03 39.84 28.66 33.46 22.28 39.84"/>
-                        <path class="cls-6" d="M40.88,64.22l5.87-3.35-5.87-3.35v6.7Z"/>
-                        <path class="cls-6" d="M52.62,64.22l-5.87-3.35,5.87-3.35v6.7Z"/>
-                        <polyline class="cls-5" points="28.57 32.11 28.57 10.99 33.41 10.99"/>
-                        <line class="cls-1" x1="28.37" y1="60.87" x2="39.64" y2="60.87"/>
-                        <line class="cls-1" x1="53.85" y1="61" x2="65.13" y2="61"/>
-                        <line class="cls-5" x1="28.37" y1="47.6" x2="28.37" y2="85.74"/>
-                      </g>           
+                    <svg id="Ebene_1" data-name="Ebene 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 117.56 174.76">
+                        <g id="Ebene_1-2" data-name="Ebene_1">
+                            <path class="cls-2" d="M32.34,1.5h52.89c17.03,0,30.84,15.23,30.84,34.02v103.73c0,18.79-13.81,34.02-30.84,34.02H32.34c-17.03,0-30.84-15.23-30.84-34.02V35.52C1.5,16.73,15.31,1.5,32.34,1.5Z"/>
+                            <line class="cls-2" x1="1.5" y1="32.34" x2="116.06" y2="32.34"/>
+                            <line class="cls-2" x1="1.5" y1="142.43" x2="116.06" y2="142.43"/>
+                            <circle class="cls-4" cx="59.01" cy="122.93" r="10.98"/>
+                            <g id="_1596143231952" data-name=" 1596143231952">
+                            <line class="cls-1" x1="33.2" y1="74.3" x2="82.6" y2="74.3"/>
+                            <line class="cls-1" x1="40.57" y1="74.3" x2="37.37" y2="68.74"/>
+                            <line class="cls-1" x1="40.57" y1="74.56" x2="37.37" y2="80.12"/>
+                            <line class="cls-3" x1="40.3" y1="73.82" x2="40.23" y2="74.57"/>
+                            <line class="cls-1" x1="75.92" y1="74.3" x2="79.13" y2="68.74"/>
+                            <line class="cls-1" x1="75.92" y1="74.56" x2="79.13" y2="80.12"/>
+                            <line class="cls-3" x1="76.19" y1="73.82" x2="76.27" y2="74.57"/>
+                            <line class="cls-1" x1="57.77" y1="99.13" x2="57.77" y2="49.73"/>
+                            <line class="cls-1" x1="57.77" y1="91.76" x2="52.21" y2="94.97"/>
+                            <line class="cls-1" x1="58.03" y1="91.76" x2="63.59" y2="94.97"/>
+                            <line class="cls-3" x1="57.3" y1="92.03" x2="58.04" y2="92.11"/>
+                            <line class="cls-1" x1="57.77" y1="56.41" x2="52.21" y2="53.2"/>
+                            <line class="cls-1" x1="58.03" y1="56.41" x2="63.59" y2="53.2"/>
+                            <line class="cls-3" x1="57.3" y1="56.14" x2="58.04" y2="56.06"/>
+                            <line class="cls-1" x1="40.34" y1="91.8" x2="75.28" y2="56.87"/>
+                            <line class="cls-1" x1="45.56" y1="86.59" x2="39.36" y2="84.93"/>
+                            <line class="cls-1" x1="45.74" y1="86.77" x2="47.4" y2="92.97"/>
+                            <line class="cls-3" x1="45.03" y1="86.45" x2="45.5" y2="87.03"/>
+                            <line class="cls-1" x1="70.55" y1="61.6" x2="68.89" y2="55.4"/>
+                            <line class="cls-1" x1="70.73" y1="61.78" x2="76.93" y2="63.44"/>
+                            <line class="cls-3" x1="70.41" y1="61.07" x2="70.99" y2="61.54"/>
+                            <line class="cls-1" x1="75.76" y1="91.99" x2="40.83" y2="57.06"/>
+                            <line class="cls-1" x1="70.55" y1="86.77" x2="68.89" y2="92.97"/>
+                            <line class="cls-1" x1="70.73" y1="86.59" x2="76.93" y2="84.93"/>
+                            <line class="cls-3" x1="70.41" y1="87.31" x2="70.99" y2="86.83"/>
+                            <line class="cls-1" x1="45.56" y1="61.78" x2="39.36" y2="63.44"/>
+                            <line class="cls-1" x1="45.74" y1="61.6" x2="47.4" y2="55.4"/>
+                            <line class="cls-3" x1="45.03" y1="61.92" x2="45.5" y2="61.35"/>
+                            </g>
+                        </g>
                     </svg>
                   </v-container>
                 </v-card-text>
@@ -82,30 +102,12 @@ export default {
   data() {
     return {
       viewBox: '0 0 400 120', // Adjust the viewBox dimensions to fit your image
-      fu: [],
-      fuEnthalten: false,
-      kältekreisAllgemein: [],
-      kältekreisAllgemeinEnthalten: false,
-      kmAnschluss: [],
-      kmAnschlussEnthalten: false,
-      klappe: [],
-      klappeEnthalten: false,
-      pumpe: [],
-      pumpeEnthalten: false,
-      raum: [],
-      raumEnthalten: false,
-      regler: [],
-      reglerEnthalten: false,
-      rücklauf: [],
-      rücklaufEnthalten: false,
-      umluftkühlgerät: [],
-      umluftkühlgerätEnthalten: false,
-      ventil: [],
-      ventilEnthalten: false,
-      vorlauf: [],
-      vorlaufEnthalten: false,
-      wmz: [],
-      wmzEnthalten: false,      
+      eisspeicher: [],
+      eisspeicherEnthalten: false,   
+      eisspeicherAnschluss: [],
+      eissspeicherAnschlussEnthalten: false,
+      kmz: [],
+      kmzEnthalten: false,
       komponenteZeigen: [],
       allComponents: null,
       allSes: null
@@ -175,54 +177,18 @@ export default {
         )
         this.allSes = allSE
     
-        if (semanticId === 'https://th-koeln.de/gart/ComponentRefrigerationCircuitGeneralAAS/1/0') {
-          this.kältekreisAllgemein = elements;
-          this.kältekreisAllgemeinEnthalten = true
-          allComponents.push('Kältekreis Allgemein')
-        } else if (semanticId === 'https://th-koeln.de/gart/ComponentFrequencyConverterAAS/1/0') {
-          this.fu = elements;
-          this.fuEnthalten = true
-          allComponents.push('Frequenzumrichter')
-        } else if (semanticId === 'https://th-koeln.de/gart/ComponentRefrigerationMachineConnectionAAS/1/0') {
-          this.kmAnschluss = elements;
-          this.kmAnschlussEnthaltenEnthalten = true
-          allComponents.push('Anschluss Kältemaschine')
-        } else if (semanticId === 'https://th-koeln.de/gart/ComponentFlapAAS/1/0') {
-          this.klappe = elements;
-          this.klappeEnthalten = true
-          allComponents.push('Klappe')
-        } else if (semanticId === 'https://th-koeln.de/gart/ComponentPumpAAS/1/0') {
-          this.pumpe = elements;
-          this.pumpeEnthalten = true
-          allComponents.push('Pumpe')
-        } else if (semanticId === 'https://th-koeln.de/gart/ComponentRoomAAS/1/0') {
-          this.raum = elements;
-          this.raumEnthalten = true
-          allComponents.push('Raum')
-        } else if (semanticId === 'https://th-koeln.de/gart/ComponentControllerAAS/1/0') {
-          this.regler = elements;
-          this.reglerEnthalten = true
-          allComponents.push('Regler')
-        } else if (semanticId === 'https://th-koeln.de/gart/ComponentReturnAAS/1/0') {
-          this.rücklauf = elements;
-          this.rücklaufEnthalten = true
-          allComponents.push('Rücklauf')
-        } else if (semanticId === 'https://th-koeln.de/gart/ComponentValveAAS/1/0') {
-          this.umluftkühlgerät = elements;
-          this.umluftkühlgerätEnthalten = true
-          allComponents.push('Ventil')
-        } else if (semanticId === 'https://th-koeln.de/gart/ComponentCirculatingAirCoolingUnitAAS/1/0') {
-          this.ventil = elements;
-          this.ventilEnthalten = true
-          allComponents.push('Ventil')
-        } else if (semanticId === 'https://th-koeln.de/gart/ComponentSupplyAAS/1/0') {
-          this.vorlauf = elements;
-          this.vorlaufEnthalten = true
-          allComponents.push('Vorlauf')
-        } else if (semanticId === 'https://th-koeln.de/gart/ComponentHeatFlowMeterAAS/1/0') {
-          this.wmz = elements;
-          this.wmzEnthalten = true
-          allComponents.push('Wärmemengenzähler')
+        if (semanticId === 'https://th-koeln.de/gart/ComponentIceTankAAS/1/0') {
+          this.eisspeicher = elements;
+          this.eisspeicherEnthalten = true
+          allComponents.push('Eisspeicher')
+        } else if (semanticId === 'https://th-koeln.de/gart/ComponentIceTankConnectionAAS/1/0') {
+          this.eisspeicherAnschluss = elements;
+          this.eissspeicherAnschlussEnthalten = true
+          allComponents.push('Eisspeicheranschluss')
+        } else if (semanticId === 'https://th-koeln.de/gart/ComponentRefrigerationMeterAAS/1/0') {
+          this.kmz = elements;
+          this.kmzEnthalten = true
+          allComponents.push('Kältemengenzähler')
         } 
       }
 
@@ -283,39 +249,32 @@ export default {
 
 <style>
 
-.cls-1, .cls-2, .cls-3, .cls-4, .cls-5, .cls-6 {
-  fill: none;
-  stroke-miterlimit: 10;
+.cls-1 {
+stroke-width: 2px;
 }
 
-.cls-1, .cls-4 {
-  stroke: #ff4a1c;
+.cls-1, .cls-2, .cls-3, .cls-4 {
+fill: none;
+stroke-miterlimit: 1.91;
 }
 
-.cls-1, .cls-5 {
-  stroke-width: 1.5px;
+.cls-1, .cls-3 {
+stroke: #372772;
 }
 
-.cls-2, .cls-4 {
-  stroke-width: 3px;
-}
-
-.cls-2, .cls-5 {
-  stroke: #372772;
+.cls-2 {
+stroke: #2b2a29;
+stroke-width: 3px;
 }
 
 .cls-3 {
-  stroke-width: .57px;
+stroke-width: .57px;
 }
 
-.cls-3, .cls-6 {
-  stroke: #2b2a29;
+.cls-4 {
+stroke: #ff4a1c;
+stroke-width: 4px;
 }
-
-.cls-6 {
-  stroke-width: 1px;
-}
-
   .pointer {
     cursor: pointer;
   }

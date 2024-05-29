4c761b4b115ec3701909a967ad5948a26764c7c8
Maximilian Both <maximilian_alexander.both@th-koeln.de>
2024-05-29 09:36:17 +0200

    add amplify v2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <Authenticator>
+      
       <template v-slot:header>
         <div style="padding: var(--amplify-space-large); text-align: center">
           <img id="auth-logo"
@@ -8,6 +9,12 @@
           />
         </div>
       </template>
+    <!--
+      <template v-slot="{ user, signOut }">
+        <h1>Hello {{ user.username }}!</h1>
+        <button @click="signOut">Sign Out</button>
+      </template>
+    -->
     </Authenticator>
       <template v-if="auth.authStatus === 'authenticated'">
         <NavBar />
@@ -41,6 +48,15 @@
 
   import { I18n } from 'aws-amplify';
   import { translations } from '@aws-amplify/ui-vue';
+
+
+  /*
+  import { signUp } from 'aws-amplify/auth';
+  import { confirmSignUp } from 'aws-amplify/auth';
+  import { autoSignIn } from 'aws-amplify/auth';
+  import { signIn } from 'aws-amplify/auth';
+  import { signOut } from 'aws-amplify/auth';
+  */
   I18n.putVocabularies(translations);
   I18n.setLanguage('de');
 
@@ -58,7 +74,65 @@
   const auth = useAuthenticator();
   const store = useGeneralStore()
   console.log(auth)
+/*
+async function handleSignUp({ username, password, email, phone_number }) {
+  try {
+    const { isSignUpComplete, userId, nextStep } = await signUp({
+      username,
+      password,
+      options: {
+        userAttributes: {
+          email,
+          phone_number // E.164 number convention
+        },
+        // optional
+        autoSignIn: true // or SignInOptions e.g { authFlowType: "USER_SRP_AUTH" }
+      }
+    });
 
+    console.log(userId);
+  } catch (error) {
+    console.log('error signing up:', error);
+  }
+}
+
+async function handleSignUpConfirmation({ username, confirmationCode }) {
+  try {
+    const { isSignUpComplete, nextStep } = await confirmSignUp({
+      username,
+      confirmationCode
+    });
+  } catch (error) {
+    console.log('error confirming sign up', error);
+  }
+}
+
+async function handleAutoSignIn() {
+  try {
+    const signInOutput = await autoSignIn();
+    // handle sign-in steps
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function signIn({ username, password }) {
+  try {
+    const { isSignedIn, nextStep } = await signIn({ username, password });
+  } catch (error) {
+    console.log('error signing in', error);
+  }
+}
+
+
+async function handleSignOut() {
+  try {
+    await signOut();
+  } catch (error) {
+    console.log('error signing out: ', error);
+  }
+}
+*/
   watchEffect(() => {
     // Check if auth.user is available and contains the necessary properties.
     if (auth.user && auth.user.signInUserSession) {

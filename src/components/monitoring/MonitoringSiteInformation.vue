60643be23105d6feb33193674104fd8909991b9c
Maximilian Both <maximilian_alexander.both@th-koeln.de>
2024-06-25 13:14:31 +0200

    change monitoring gebäude

--- src/components/monitoring/MonitoringSiteInformation.vue
+++ src/components/monitoring/MonitoringSiteInformation.vue
@@ -3,7 +3,8 @@
         <v-card 
             style="border-radius: 20px; background-color: whitesmoke"
             variant="outlined" class="anlagen-card">
-            <v-card-title>Liegenschaft - Performance</v-card-title>
+            <v-card-title align="center">Liegenschaft - Performance</v-card-title>
+            <v-divider class="border-opacity-75 mx-4 mb-2" :thickness="2" color="success"></v-divider>
             <v-card-text>
                 <KPIsSite />
             </v-card-text>

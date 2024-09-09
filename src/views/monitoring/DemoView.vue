bdd664a1d92ec63db58b47ade4749db2a3d22477
bernhardkelm <me@bernhardkelm.de>
2024-09-09 10:16:05 +0200

    Small linting fixes

--- src/views/monitoring/DemoView.vue
+++ src/views/monitoring/DemoView.vue
@@ -11,7 +11,9 @@
             variant="outlined"
             class="anlagen-card mb-12 mx-10"
           >
-            <v-card-title align="center">Meine Liegenschaften - Monitoring</v-card-title>
+            <v-card-title align="center">
+              Meine Liegenschaften - Monitoring
+            </v-card-title>
             <v-divider
               class="border-opacity-75 mx-4 mb-2"
               :thickness="2"
@@ -19,16 +21,16 @@
             ></v-divider>
 
             <LiegenschaftCard
-            location="Köln"
-            name="TH Köln, Campus Deutz"
+              location="Köln"
+              name="TH Köln, Campus Deutz"
             />
             <LiegenschaftCard
-            location="Köln"
-            name="TH Köln, Campus Gummersbach"
+              location="Köln"
+              name="TH Köln, Campus Gummersbach"
             />
             <LiegenschaftCard
-            location="Köln"
-            name="TH Köln, Campus Deutz"
+              location="Köln"
+              name="TH Köln, Campus Deutz"
             />
             <StatusCard
               icon="mdi-check-circle"
@@ -64,8 +66,6 @@
   </div>
 </template>
 <script>
-import { useGeneralStore } from '@/store/general'
-
 import GoogleMapsCardHome from '@/components/general/GoogleMapsCardHome.vue'
 import LiegenschaftCard from '@/components/monitoring/LiegenschaftCard.vue'
 import StatusCard from '@/components/general/StatusCard.vue'
@@ -75,22 +75,7 @@ export default {
     GoogleMapsCardHome,
     LiegenschaftCard,
     StatusCard
-    //AmCharts, PieChart
   },
-  computed: {
-    generalStore() {
-      return useGeneralStore()
-    }
-  },
-  created() {
-    const site_id = this.$route.params.siteid
-    for (let site in this.generalStore.loadedSiteInformationWithBuildings) {
-      if (site_id === this.generalStore.loadedSiteInformationWithBuildings[site]['siteName']) {
-        console.log(this.generalStore.loadedSiteInformationWithBuildings[site])
-        this.buildings = this.generalStore.loadedSiteInformationWithBuildings[site].buildings
-      }
-    }
-  }
 }
 </script>
 <style lang="">

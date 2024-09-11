e877dfdcefd93786ae2c259e57c3684e3942fbbd
bernhardkelm <me@bernhardkelm.de>
2024-09-11 12:12:21 +0200

    Added interface type declarations

--- src/types/Company.ts
+++ src/types/Company.ts
@@ -1,3 +1,15 @@
+/**
+ * Company interface
+ * @typedef {Object} Company
+ * @property {string} id - The unique identifier of the company
+ * @property {Object} data - The data of the company
+ * @property {string} data.CompanyName - The name of the company
+ * @property {Object[]} data.Address - The address of the company
+ * @property {string} data.Address.Street - The street of the company
+ * @property {string} data.Address.Zipcode - The zipcode of the company
+ * @property {string} data.Address.CityTown - The city or town of the company
+ * @property {string} data.Address.NationalCode - The national code of the company
+ */
 export default interface Company {
   id: string;
   data: {

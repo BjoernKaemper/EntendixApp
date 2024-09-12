c676d3b304d04d3949bbacc48a72803345fa5e2f
Philipp Rudolph <p.rudolph@klickmeister.de>
2024-09-12 16:43:27 +0200

    add Big Number, Line Chart v2 and demo for monitoring site

---
+++ src/components/general/BigNumber.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="big-number">
+    {{ number || '???' }}
+    <p class="unit">{{ unit }}</p>
+  </div>
+</template>
+<script langs="ts">
+export default {
+  props: {
+    number: {
+      type: Number,
+      default: 0
+    },
+    unit: {
+      type: String,
+      default: 'tbd.'
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import '@/styles/mixins.scss';
+
+  .big-number {
+    @include big-number;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: end
+  }
+  .unit {
+    @include content;
+    margin-left: $m
+  }
+
+</style>
---
+++ src/components/monitoring/LineChart_v2.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="line-chart-container">
+    <div class="line-chart-container--left">
+      <h3>{{ topic }}</h3>
+      <BigNumber number="135,27" unit="kWh/m2/a" />
+       <p class="last-update">Letzte Aktualisierung vor: {{ lastUpdate }} Minuten</p>
+    </div>
+    <div class="line-chart-container--right">
+      <p>insert Line Chart here...</p>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import BigNumber from '@/components/general/BigNumber.vue';
+export default {
+  props: {
+    topic: {
+      type: String,
+      required: true,
+      default: 'Line Chart',
+    },
+  },
+  components: {
+    BigNumber,
+  },
+  data() {
+    return {
+      lastUpdate: 0,
+    };
+  },
+
+};
+</script>
+<style lang="scss">
+@import '@/styles/mixins.scss';
+
+  .line-chart-container {
+    width: 100%;
+    height: 35%;
+    background-color: $lightest;
+    border-radius: $base-size;
+    padding: $m;
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+
+    > .line-chart-container--left {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+    > .line-chart-container--right {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+
+
+  .last-update {
+    @include meta-information;
+  }
+</style>

4531cff8e4845ad553c6d2671d831228cbae7d9e
bernhardkelm <me@bernhardkelm.de>
2024-09-18 16:08:08 +0200

    Removed old eslint plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,12 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-import eslint from 'vite-plugin-eslint';
 import checker from 'vite-plugin-checker';
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    eslint(),
     checker({
       typescript: true,
     }),

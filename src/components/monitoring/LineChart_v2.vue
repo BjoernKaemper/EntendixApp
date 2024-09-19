c0b56509fd94e391ad6bd7daa7b23d57abbae14d
BenediktEngel <b.engel@klickmeister.de>
2024-09-19 13:30:54 +0200

    feat: add Kpis to buildign view

--- src/components/monitoring/LineChart_v2.vue
+++ src/components/monitoring/LineChart_v2.vue
@@ -2,11 +2,11 @@
   <div class="line-chart-container">
     <div class="line-chart-container--left">
       <h3>{{ topic }}</h3>
-      <BigNumber number="135,27" unit="kWh/m2/a" />
+      <BigNumber :number="value" :unit="unit" />
       <p class="last-update">Letzte Aktualisierung vor: {{ lastUpdate }} Minuten</p>
     </div>
     <div class="line-chart-container--right">
-      <p> @TODO: insert Line Chart here... </p>
+      <p>@TODO: insert Line Chart here...</p>
     </div>
   </div>
 </template>
@@ -26,6 +26,26 @@ export default {
       required: true,
       default: 'Line Chart',
     },
+    /**
+     * The unit of the value
+     * @type {String}
+     * @default 'kWh/m2/a'
+     */
+    unit: {
+      type: String,
+      required: false,
+      default: 'kWh/m2/a',
+    },
+    /**
+     * The value of the line chart
+     * @type {String}
+     * @default '0'
+     */
+    value: {
+      type: String,
+      required: true,
+      default: '0',
+    },
   },
   components: {
     BigNumber,
@@ -36,34 +56,33 @@ export default {
     };
   },
 };
-
 </script>
 
 <style lang="scss">
 @import '@/styles/mixins.scss';
 
-  .line-chart-container {
-    width: 100%;
-    height: 35%;
-    background-color: $lightest;
-    border-radius: $base-size;
-    padding: $m;
-    display: grid;
-    grid-template-columns: 1fr 2fr;
+.line-chart-container {
+  width: 100%;
+  height: 35%;
+  background-color: $lightest;
+  border-radius: $base-size;
+  padding: $m;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
 
-    > .line-chart-container--left {
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-    }
-    > .line-chart-container--right {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
+  > .line-chart-container--left {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
   }
+  > .line-chart-container--right {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
 
-  .last-update {
-    @include meta-information;
-  }
+.last-update {
+  @include meta-information;
+}
 </style>

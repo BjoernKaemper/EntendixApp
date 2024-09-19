65f9f99f9810b4ff1ce68424e790d89f2e716579
BenediktEngel <b.engel@klickmeister.de>
2024-09-19 12:35:57 +0200

    feat: use middleware with proxy

--- src/store/general_v2.ts
+++ src/store/general_v2.ts
@@ -46,13 +46,13 @@ export const useGeneralStoreV2 = defineStore('general_v2', {
       } as RequestInit;
 
       this.sites = await FetchHelper.apiCall(
-        `${import.meta.env.VITE_MIDDLEWARE_URL}/sites?${q}`,
+        `/middleware/sites?${q}`,
         requestOptions,
       ) as Site[];
 
       // Fetching types Company Information
       this.companies = await FetchHelper.apiCall(
-        `${import.meta.env.VITE_MIDDLEWARE_URL}/companies?${q}`,
+        `/middleware/companies?${q}`,
         requestOptions,
       ) as Company[];
 
@@ -72,7 +72,7 @@ export const useGeneralStoreV2 = defineStore('general_v2', {
       } as RequestInit;
 
       this.currentSite = await FetchHelper.apiCall(
-        `${import.meta.env.VITE_MIDDLEWARE_URL}/sites/${siteId}?${q}`,
+        `/middleware/sites/${siteId}?${q}`,
         requestOptions,
       ) as SiteWithBuildinginformation;
 

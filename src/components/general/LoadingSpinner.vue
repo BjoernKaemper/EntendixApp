3d2d79c5c7018290c169e1387e473be4e2d86980
bernhardkelm <me@bernhardkelm.de>
2024-09-24 15:30:49 +0200

    Added loading spinner to liegenschaften view

---
+++ src/components/general/LoadingSpinner.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="loading--wrapper">
+    Loading........
+  </div>
+</template>
+
+<script lang="ts">
+/**
+ * A general loading spinner component
+ * @module components/general/LoadingSpinner
+ * @displayName LoadingSpinner
+ */
+
+export default {
+  name: 'LoadingSpinner',
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>

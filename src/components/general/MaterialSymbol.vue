d07a46172c9f478f0f11f30e439315076d02f2ab
Nils Polarek <n.polarek@klickmeister.de>
2024-10-10 18:06:58 +0200

    use fontloader to load material symbol and add material symbols

---
+++ src/components/general/MaterialSymbol.vue
@@ -0,0 +1,21 @@
+<template>
+  <span class="material-symbols-outlined">{{ symbol }}</span>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'MaterialSymbol',
+  props: {
+    /**
+     * The symbol to display.
+     * @type {string}
+     * @required
+     */
+    symbol: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>

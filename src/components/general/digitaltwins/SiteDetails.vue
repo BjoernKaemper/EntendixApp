287e0beced747282a2b939584d05805c71faab63
Nils Polarek <n.polarek@live.de>
2024-10-18 12:57:28 +0200

    use new input handling on site details

--- src/components/general/digitaltwins/SiteDetails.vue
+++ src/components/general/digitaltwins/SiteDetails.vue
@@ -5,17 +5,55 @@
       src="@/assets/placeholder-campus-deutz.png"
       :alt="`image of ${site.data.siteName}`"
     />
-    <form class="site-detail__info" @submit.prevent="console.log('TODO')" @focusin="formFocused = true" @focusout="formFocused = false">
+    <form
+      class="site-detail__info"
+      @submit.prevent="console.log('TODO')"
+      @focusin="formFocused = true"
+      @focusout="formFocused = false"
+    >
       <h3>Informationen über die Liegenschaft</h3>
-      <FormInput id="street-input" label="Straße" placeholder="Straße" v-model="street" />
+      <FormInput
+        id="street-input"
+        label="Straße"
+        placeholder="Straße"
+        v-model="streetInput.value.value"
+        :hasError="!streetInput.isValid"
+        :errorMessage="streetInput.errorMessage.value"
+      />
       <div class="site-detail__group">
-        <FormInput id="plz-input" label="PLZ" placeholder="PLZ" v-model="zipCode" />
-        <FormInput id="city-input" label="Stadt" placeholder="Stadt" v-model="city" />
+        <FormInput
+          id="plz-input"
+          label="PLZ"
+          placeholder="PLZ"
+          v-model="zipCodeInput.value.value"
+          :hasError="!zipCodeInput.isValid"
+          :errorMessage="zipCodeInput.errorMessage.value"
+        />
+        <FormInput
+          id="city-input"
+          label="Stadt"
+          placeholder="Stadt"
+          v-model="cityInput.value.value"
+          :hasError="!cityInput.isValid"
+          :errorMessage="cityInput.errorMessage.value"
+        />
       </div>
-      <FormInput id="land-input" label="Land" placeholder="Land" v-model="country" />
+      <FormInput
+        id="country-input"
+        label="Land"
+        placeholder="Land"
+        v-model="countryInput.value.value"
+        :hasError="!countryInput.isValid"
+        :errorMessage="countryInput.errorMessage.value"
+      />
       <div class="site-detail__form-actions" v-if="formFocused">
         <ButtonComponent type="reset" text="Abbrechen" state="tertiary" />
-        <ButtonComponent type="submit" text="Speichern" state="primary" :icon="IconTypes.CHECK_MARK" />
+        <ButtonComponent
+          type="submit"
+          text="Speichern"
+          state="primary"
+          :icon="IconTypes.CHECK_MARK"
+        />
       </div>
     </form>
   </div>
@@ -25,13 +63,20 @@
 // TODO: connect to backend
 // TODO: determine what should happen on unsaved changes
 
+// Hook import
+import { useInput } from '@/hooks/useInput';
+import { useFormManager } from '@/hooks/useFormManager';
+
 // Component imports
 import FormInput from '@/components/general/forms/FormInput.vue';
 import ButtonComponent from '@/components/general/ButtonComponent.vue';
 
+// Helper imports
+import { requiredValidator } from '@/helpers/FormValidators';
+
 // Type imports
 import type { SiteWithBuildinginformation } from '@/types/global/site/Site';
-import type { PropType } from 'vue';
+import { type PropType } from 'vue';
 import { IconTypes } from '@/types/enums/IconTypes';
 
 export default {
@@ -46,6 +91,25 @@ export default {
       required: true,
     },
   },
+  setup(props) {
+    const streetInput = useInput<string>([requiredValidator], props.site.data.address.street);
+    const zipCodeInput = useInput<string>([requiredValidator], props.site.data.address.zipcode);
+    const cityInput = useInput<string>([requiredValidator], props.site.data.address.cityTown);
+    const countryInput = useInput<string>(
+      [requiredValidator],
+      props.site.data.address.nationalCode,
+    );
+
+    const formState = useFormManager([streetInput, zipCodeInput, cityInput, countryInput]);
+
+    return {
+      streetInput,
+      zipCodeInput,
+      cityInput,
+      countryInput,
+      formState,
+    };
+  },
   data() {
     return {
       IconTypes,
@@ -55,6 +119,8 @@ export default {
       city: '',
       country: '',
       test: '',
+      // TODO: handle focus properly, when one looses focus and another one gets
+      // it, the state flaps
       formFocused: false,
     };
   },

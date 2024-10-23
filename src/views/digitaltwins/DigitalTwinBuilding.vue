a6f571fdedbe6682517555280e93f36b343d1cac
Nils Polarek <n.polarek@live.de>
2024-10-23 16:18:54 +0200

    add loading view to level 3

--- src/views/digitaltwins/DigitalTwinBuilding.vue
+++ src/views/digitaltwins/DigitalTwinBuilding.vue
@@ -1,12 +1,30 @@
 <template>
   <DigitalTwinLayout>
     <template #left>
-      <h2>{{ buildingName }}</h2>
-      <BuildingDetails v-if="building" :buildingName="buildingName" :building="building" />
+      <h1>{{ buildingName }}</h1>
+      <div v-if="buildingLoading" class="digital-twin-building__loading">
+        <LoadingCards
+          :cardCount="1"
+          cardClass="digital-twin-building__image-loading"
+          size="large"
+        />
+        <h3>Informationen über das Gebäude</h3>
+        <LoadingSpinner />
+      </div>
+      <BuildingDetails
+        v-else-if="!buildingLoading && building"
+        :buildingName="buildingName"
+        :building="building"
+      />
     </template>
     <template #right>
       <h2>Verwaltung des Gebäudes</h2>
-      <div class="digital-twin-building__trades">
+      <LoadingSpinner
+        v-if="subSectionsLoading"
+        size="large"
+        class="digital-twin-building__trades-loading"
+      />
+      <div v-else class="digital-twin-building__trades">
         <TradeCard
           v-for="(trade, idx) in trades"
           :key="idx"
@@ -22,7 +40,6 @@
 </template>
 
 <script lang="ts">
-// TODO: loading state
 // TODO: Building settings modal/button
 // TODO: Building details form handling
 // TODO: Edge device currently gathering data view
@@ -40,6 +57,8 @@ import { useBuildingStore } from '@/store/building';
 import DigitalTwinLayout from '@/components/digitaltwins/DigitalTwinLayout.vue';
 import BuildingDetails from '@/components/digitaltwins/BuildingDetails.vue';
 import TradeCard from '@/components/digitaltwins/TradeCard.vue';
+import LoadingSpinner from '@/components/general/LoadingSpinner.vue';
+import LoadingCards from '@/components/general/LoadingCards.vue';
 
 // config import
 import { tradesConfig } from '@/configs/trades';
@@ -49,6 +68,8 @@ export default {
     DigitalTwinLayout,
     BuildingDetails,
     TradeCard,
+    LoadingSpinner,
+    LoadingCards,
   },
   data() {
     return {
@@ -69,9 +90,11 @@ export default {
     },
 
     subSectionsLoading() {
-      // Check for building and subsection loading state, as subsections are
-      // loaded after the building
-      return this.buildingStore.isLoading || this.buildingStore.subsectionState.isLoading;
+      return this.buildingStore.subsectionState.isLoading;
+    },
+
+    buildingLoading() {
+      return this.buildingStore.isLoading;
     },
 
     trades() {
@@ -143,15 +166,31 @@ export default {
     grid-template-columns: repeat(auto-fill, minmax(325px, 1fr));
     gap: $m;
   }
+
+  &__loading {
+    display: flex;
+    flex-direction: column;
+    gap: $s;
+  }
+
+  &__trades-loading {
+    height: auto;
+    padding: $xxl $m;
+  }
 }
 
-h1 {
-  @include content-headline;
+:deep(.digital-twin-building__image-loading) {
+  aspect-ratio: 4 / 5;
+  height: 100%;
 }
 
-h2,
-h3 {
+h1,
+h2 {
   @include content-headline;
   color: $dark-green;
+}
+
+h3 {
+  @include content-subtitle;
 }
 </style>

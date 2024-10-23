7eb59b35d4a2e4be19720cbaa7b98ac3fd13495c
BenediktEngel <b.engel@klickmeister.de>
2024-10-23 16:38:50 +0200

    docs: add todo for tooltip and remove unused code

--- src/components/general/BigNumber.vue
+++ src/components/general/BigNumber.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="big-number">
     {{ number === null ? '???' : number }}
+    <!-- TODO: Tooltip with textversion of the unit (e.g. Kilowattstunde pro Quadratmeter) -->
     <p class="unit">{{ unit }}</p>
   </div>
 </template>
@@ -21,37 +22,33 @@ export default {
     /**
      * The unit of the number
      * @type {String}
-     * @default 'tbd.'
+     * @default ''
      */
     unit: {
       type: String as PropType<string>,
-      default: 'tbd.',
+      default: '',
     },
   },
 };
-
 </script>
 
 <style lang="scss" scoped>
-@import '@/styles/mixins.scss';
+.big-number {
+  @include big-number;
+  width: 100%;
+  display: flex;
+  align-items: end;
+  justify-content: end;
+  opacity: 50%;
+  transition: opacity 0.2s;
+  padding: $m;
 
-  .big-number {
-    @include big-number;
-    width: 100%;
-    display: flex;
-    align-items: end;
-    justify-content: end;
-    opacity: 50%;
-    transition: opacity 0.2s;
-    padding: $m;
-
-    &:hover {
-      opacity: 100%;
-    }
+  &:hover {
+    opacity: 100%;
   }
-  .unit {
-    @include content;
-    margin-left: $m;
-  }
-
+}
+.unit {
+  @include content;
+  margin-left: $m;
+}
 </style>

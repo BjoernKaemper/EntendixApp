5b1190902a8b7d8e9401af00b495856d0cf5dc3a
BenediktEngel <b.engel@klickmeister.de>
2024-10-28 11:30:06 +0100

    feat: add interception Modal and form handling WIP

--- src/components/general/modals/AddSiteOverlayModal.vue
+++ src/components/general/modals/AddSiteOverlayModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <ModalOverlay :isOpen="isAddSiteModalOpen">
+  <ModalOverlay :isOpen="isAddSiteModalOpen" @close="resetSite">
     <template #header>
       <h3>Neue Liegenschaft anlegen</h3>
     </template>
@@ -8,49 +8,59 @@
         Geben Sie die Informationen einer neuen Liegenschaft ein, um sie innerhalb von ENTENDIX zu
         verwalten.
       </p>
-      <form action="">
+      <form ref="form" @submit.prevent="console.log('TODO')" @reset="closeAndResetForm">
         <h4>Informationen pflegen</h4>
         <FormInput
           id="name"
           label="Name"
-          placeholder=""
+          placeholder="Name der Liegenschaft"
           :required="true"
           type="text"
-          :model-value="name"
+          v-model="nameInput.value.value"
+          :hasError="!nameInput.isValid.value"
+          :errorMessage="nameInput.errorMessage.value"
         />
         <FormInput
           id="street"
           label="Straße"
-          placeholder=""
+          placeholder="Straße"
           :required="true"
           type="text"
-          :model-value="street"
+          v-model="streetInput.value.value"
+          :hasError="!streetInput.isValid.value"
+          :errorMessage="streetInput.errorMessage.value"
         />
         <div class="form-grid">
           <FormInput
             id="zip"
             label="PLZ"
-            placeholder=""
+            placeholder="PLZ"
             :required="true"
             type="text"
-            :model-value="zip"
+            v-model="zipCodeInput.value.value"
+            :hasError="!zipCodeInput.isValid.value"
+            :errorMessage="zipCodeInput.errorMessage.value"
           />
           <FormInput
             id="city"
             label="Stadt"
-            placeholder=""
+            placeholder="Stadt"
             :required="true"
             type="text"
-            :model-value="city"
+            v-model="cityInput.value.value"
+            :hasError="!cityInput.isValid.value"
+            :errorMessage="cityInput.errorMessage.value"
           />
         </div>
         <FormInput
           id="country"
           label="Land"
-          placeholder=""
+          placeholder="Land"
           :required="true"
           type="text"
-          :model-value="country"
+          v-model="countryInput.value.value"
+          :hasError="!countryInput.isValid.value"
+          :errorMessage="countryInput.errorMessage.value"
         />
         <FileInput
           id="image"
@@ -61,23 +71,41 @@
       </form>
     </template>
     <template #footer>
-      <ButtonComponent @click="close" text="Abbrechen" />
+      <ButtonComponent type="reset" text="Abbrechen" state="secondary" @click="resetSite" />
       <ButtonComponent
-        @click="addSite"
-        :primary="true"
+        type="submit"
         text="Liegenschaft anlegen"
+        state="primary"
         :icon="IconTypes.CHECK_MARK"
+        @click="addSite"
       />
     </template>
   </ModalOverlay>
+  <InterceptionModal
+    :isOpen="leaveFormInterception.isOpen.value"
+    @cancel="leaveFormInterception.abortAction"
+    @confirm="leaveFormInterception.confirmAction"
+  />
 </template>
 
 <script lang="ts">
+// Hook import
+import { useInput } from '@/hooks/useInput';
+import { useFormManager } from '@/hooks/useFormManager';
+import { useModalInterception } from '@/hooks/useModalInterception';
+import { usePageLeaveInterception } from '@/hooks/usePageLeaveInteception';
+
+// Component import
 import ButtonComponent from '@/components/general/ButtonComponent.vue';
 import ModalOverlay from '@/components/general/modals/ModalOverlay.vue';
 import FormInput from '@/components/general/forms/FormInput.vue';
 import FileInput from '@/components/general/forms/FileInput.vue';
+import InterceptionModal from '@/components/general/modals/InterceptionModal.vue';
 
+// Helper imports
+import { requiredValidator } from '@/helpers/FormValidators';
+
+// Type imports
 import { IconTypes } from '@/types/enums/IconTypes';
 
 export default {
@@ -86,6 +114,7 @@ export default {
     ButtonComponent,
     FormInput,
     FileInput,
+    InterceptionModal,
   },
   data() {
     return {
@@ -110,21 +139,46 @@ export default {
     },
   },
   setup() {
+    const nameInput = useInput<string>([requiredValidator], '');
+    const streetInput = useInput<string>([requiredValidator], '');
+    const zipCodeInput = useInput<string>([requiredValidator], '');
+    const cityInput = useInput<string>([requiredValidator], '');
+    const countryInput = useInput<string>([requiredValidator], '');
+    // TODO: IMAGEinput
+
+    const formState = useFormManager([streetInput, zipCodeInput, cityInput, countryInput]);
+
+    const leaveFormInterception = useModalInterception();
+
+    usePageLeaveInterception(formState.isChanged, leaveFormInterception.interceptAction);
+
     return {
+      nameInput,
+      streetInput,
+      zipCodeInput,
+      cityInput,
+      countryInput,
+      formState,
+      leaveFormInterception,
       IconTypes,
     };
   },
   computed: {},
   methods: {
-    close() {
-      this.$emit('close');
-    },
     addSite() {
       // TODO handle check if all fields are filled
       // TODO handle the adding
+      this.$refs.form.submit();
+    },
+    resetSite() {
+      this.$refs.form.reset();
     },
     setImage(files: File[]) {
       this.image = files;
+    },
+    closeAndResetForm() {
+      this.formState.reset();
+      this.$emit('close');
     },
   },
 };

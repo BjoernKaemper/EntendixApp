ab08128b237db21fa1c9e6c3428189ea55b60558
BenediktEngel <b.engel@klickmeister.de>
2024-10-30 11:47:49 +0100

    feat: add formhandling for comments

--- src/components/general/comments/CommentsContainer.vue
+++ src/components/general/comments/CommentsContainer.vue
@@ -12,38 +12,74 @@
     </button>
   </div>
   <div>
-    <!-- TODO: Styling of Label is off -->
-    <FormInput
-      id="newComment"
-      label="Kommentar hinzufügen"
-      placeholder="Nachricht"
-      :required="true"
-      type="textarea"
-      model-value=""
-      icon="notes"
-    />
+    <form ref="form" @submit.prevent="handleSubmit">
+      <!-- TODO: Styling of Label is off -->
+      <FormInput
+        id="comment"
+        icon="notes"
+        type="textarea"
+        label="Kommentar hinzufügen"
+        placeholder="Nachricht"
+        :hasError="!newComment.isValid && formState.showErrors.value"
+        :error-message="formState.showErrors.value ? newComment.errorMessage.value : undefined"
+        v-model="newComment.value.value"
+        required
+        :rows="3"
+      />
+    </form>
   </div>
   <ButtonComponent
+    type="submit"
+    @click="submitForm"
     text="Kommentar hinzufügen"
     state="primary"
     :icon="IconTypes.ADD"
     class="add-comment-button"
+    title="Coming soon"
+    disabled
   />
+  <InterceptionModal
+    :isOpen="interceptLeave.isOpen.value"
+    @cancel="interceptLeave.abortAction"
+    @confirm="interceptLeave.confirmAction"
+    title="Änderungen verwerfen?"
+    confirmText="Änderungen verwerfen"
+  >
+    <template #body>
+      <p>
+        Sind Sie sicher, dass Sie den aktuellen Dialog schließen wollen? Die von Ihnen vorgenommenen
+        <strong>Änderungen gehen verloren.</strong>
+      </p>
+    </template>
+  </InterceptionModal>
 </template>
 
 <script lang="ts">
+// Hook imports
+import { useInput } from '@/hooks/useInput';
+import { useFormManager } from '@/hooks/useFormManager';
+import { useModalInterception } from '@/hooks/useModalInterception';
+import { usePageLeaveInterception } from '@/hooks/usePageLeaveInteception';
+
+// Type imports
 import { IconTypes } from '@/types/enums/IconTypes';
 import type { Annotation } from '@/types/global/kpi/Kpi';
 
+// Helper imports
+import { requiredValidator } from '@/helpers/FormValidators';
+
+// Component imports
 import CommentsWrapper from '@/components/general/comments/CommentsWrapper.vue';
 import ButtonComponent from '@/components/general/ButtonComponent.vue';
 import FormInput from '@/components/general/forms/FormInput.vue';
+import InterceptionModal from '@/components/general/modals/InterceptionModal.vue';
 
 export default {
   components: {
     CommentsWrapper,
     FormInput,
     ButtonComponent,
+    InterceptionModal,
   },
 
   props: {
@@ -67,6 +103,45 @@ export default {
           ? this.commentCount + 2
           : this.comments.length;
     },
+    submitForm() {
+      if (!this.$refs.form) {
+        return;
+      }
+
+      const form = this.$refs.form as HTMLFormElement;
+
+      form.requestSubmit();
+    },
+    handleClose() {
+      if (!this.formState.isChanged.value) {
+        return;
+      }
+
+      this.interceptLeave.interceptAction(
+        () => {
+          this.formState.reset();
+        },
+        () => {},
+      );
+    },
+    handleSubmit() {
+      if (!this.formState.isValid.value) {
+        this.formState.showErrors.value = true;
+        return;
+      }
+
+      const user: string =
+        window.localStorage.getItem(
+          'CognitoIdentityServiceProvider.72jdgrgeu89hiqvmaciibrdi4.LastAuthUser',
+        ) || 'User'; // @TODO get user from backend
+      const dateOfSubmission = new Date().toISOString().split('T')[0];
+
+      // TODO: submit properly
+      const body = new FormData();
+      body.append('comment', this.newComment.value.value);
+      body.append('user', user);
+      body.append('dateOfSubmission', dateOfSubmission);
+    },
   },
   computed: {
     currentComments() {
@@ -74,7 +149,17 @@ export default {
     },
   },
   setup() {
+    const newComment = useInput<string>([requiredValidator], '');
+
+    const formState = useFormManager([newComment]);
+    const interceptLeave = useModalInterception();
+
+    usePageLeaveInterception(formState.isChanged, interceptLeave.interceptAction);
+
     return {
+      newComment,
+      formState,
+      interceptLeave,
       IconTypes,
     };
   },

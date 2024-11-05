f255ecc068995178e7d318a462acc121d6bb0fd7
Nils Polarek <n.polarek@live.de>
2024-11-05 18:09:41 +0100

    allow updating of initialvalue for inputs, use 'watchEffect' to react to multiple vars

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,4 +1,4 @@
-import { ref, watch, type Ref } from 'vue';
+import { ref, watchEffect, type Ref } from 'vue';
 import type { EntendixInput, FormValidator } from '@/types/local/Inputs';
 
 /**
@@ -11,6 +11,7 @@ export const useInput = <T = string | number>(
   validators: FormValidator<T>[],
   initialValue: T,
 ): EntendixInput<T> => {
+  const internalInitialValue = ref<T>(initialValue) as Ref<T>;
   const value = ref<T>(initialValue) as Ref<T>;
   const isValid = ref(false);
   const isChanged = ref(false);
@@ -18,48 +19,51 @@ export const useInput = <T = string | number>(
 
   // Aware that this could be computed values, but these somehow don't work in
   // conjunction with the options api, so went for refs and a watch instead
-  watch(
-    value,
-    () => {
-      if (Array.isArray(initialValue) && Array.isArray(value.value)) {
-        if (initialValue.length !== value.value.length) {
-          isChanged.value = true;
-        } else {
-          isChanged.value = initialValue.some(
-            (val, index) => val !== value.value[index as keyof T],
-          );
-        }
-      } else if ((!initialValue && value.value) || value.value !== initialValue) {
+  watchEffect(() => {
+    if (Array.isArray(internalInitialValue.value) && Array.isArray(value.value)) {
+      if (internalInitialValue.value.length !== value.value.length) {
         isChanged.value = true;
       } else {
-        isChanged.value = false;
+        isChanged.value = internalInitialValue.value.some(
+          (val, index) => val !== value.value[index as keyof T],
+        );
       }
+    } else if (
+      (!internalInitialValue.value && value.value) ||
+      value.value !== internalInitialValue.value
+    ) {
+      isChanged.value = true;
+    } else {
+      isChanged.value = false;
+    }
 
-      const errorMessages: string[] = [];
-      isValid.value = true;
-      errorMessage.value = undefined;
+    const errorMessages: string[] = [];
+    isValid.value = true;
+    errorMessage.value = undefined;
 
-      validators.forEach((validator) => {
-        const validatorResult = validator.fn(value.value as T);
+    validators.forEach((validator) => {
+      const validatorResult = validator.fn(value.value as T);
 
-        if (!validatorResult) {
-          errorMessages.push(validator.errorMessage);
-          isValid.value = false;
-        }
-      });
-
-      if (errorMessages.length) {
-        errorMessage.value = errorMessages.join(', ');
+      if (!validatorResult) {
+        errorMessages.push(validator.errorMessage);
+        isValid.value = false;
       }
-    },
-    { immediate: true },
-  );
+    });
+
+    if (errorMessages.length) {
+      errorMessage.value = errorMessages.join(', ');
+    }
+  });
 
   /**
    * Reset the input to its initial value
    */
   const reset = () => {
-    value.value = initialValue;
+    value.value = internalInitialValue.value;
+  };
+
+  const updateInitialValue = (newValue: T) => {
+    internalInitialValue.value = newValue;
   };
 
   return {
@@ -68,5 +72,6 @@ export const useInput = <T = string | number>(
     isValid,
     errorMessage,
     reset,
+    updateInitialValue,
   };
 };
